138783315145a3a0a499fafd8f39f64a4351f526
Tee-Stark <59034168+Tee-Stark@users.noreply.github.com>
2021-08-13 00:56:23 +0100

    added membership subscription endpoint

--- routes/customers.js
+++ routes/customers.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authorize = require("../middlewares/auth.js");
 const customers = require("../controllers/customers.js");
-const subscription = require("../controllers/subscriptions.js");
+const mailing = require("../controllers/subscriptions.js");
 
 router.post("/signup", customers.createCustomer);
 router.post("/login", customers.loginCustomer);
 router.put("/:id", authorize, customers.updateCustomer);
 router.get("/", customers.getAllCustomers);
 router.get("/:id", customers.getCustomerById);
-router.post("/subscribe", subscription.mailSubscribe);
+router.post("/subscription/:id", customers.subscription);
+router.post("/mailing", mailing.mailSubscribe);
 router.delete("/:id", authorize, customers.deleteCustomer);
 
 module.exports = router;
